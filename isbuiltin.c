c204ebfe9925382f015288e552da4899eed7e569
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-04-01 14:49:40 -0500

    builtin function built

---
+++ isbuiltin.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * isbuiltin - function for calling our builtin functions
+ * @buffer: user input
+ * @environ: environmental variables
+ * 
+ * Description: If 'exit' is sent to the shell then shell 
+ * terminates its own process
+ * 
+ * Return: if not builtin return 0, if builtin return 1
+*/
+
+int isbuiltin(char *buffer, char **environ)
+{
+	int i;
+	if (!strcmp(buffer, "exit\n"))
+	{
+		exit(EXIT_SUCCESS);
+	}
+
+	if (!strcmp(buffer, "env\n"))
+	{
+		for (i = 0; environ[i]; i++)
+		{
+			printf("%s\n", environ[i]);
+		}
+		return (1);
+	}
+	return (0);
+}
