86ee7402042e9e69a26d9c1aa0dc99d654615e24
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-04-02 10:06:06 -0500

    new docu

--- fork_process_and_execute.c
+++ fork_process_and_execute.c
@@ -1,6 +1,15 @@
 #include "shell.h"
 #include <sys/wait.h>
 
+/**
+ * fork_process_and_execute - forks process and executes
+ * @path: path name of the program to be run
+ * @argv: Arguments sent to the shell
+ * @environ: list of environmental variables
+ * 
+ * Return: status that child exited with
+*/
+
 int fork_process_and_execute(char *path, char *argv[], char *environ[])
 {
 	pid_t myPid;
